1fda7ae096e11760c082f6c7d27035f9c3ffea0e
Dénes Solti <sodnaatx@gmail.com>
2020-06-29 18:12:43 +0200

    [add feature] ModuleRegistry

---
+++ SRC/AppHost/Private/ModuleRegistry.cs
@@ -0,0 +1,20 @@
+﻿/********************************************************************************
+* ModuleRegistry.cs                                                             *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+
+namespace Solti.Utils.AppHost.Internals
+{
+    using DI;
+    using DI.Interfaces;
+
+    internal sealed class ModuleRegistry : IModuleRegistry
+    {
+        public IServiceContainer Container { get; }
+
+        public ModuleRegistry(IServiceContainer container) => Container = container;
+
+        public void Register<TInterface, TImplementation>() where TInterface : class where TImplementation : TInterface => Container.Service<TInterface, TImplementation>(Lifetime.Scoped);
+    }
+}
---
+++ SRC/AppHost/Public/IModuleRegistry.cs
@@ -0,0 +1,19 @@
+﻿/********************************************************************************
+* IModuleRegistry.cs                                                            *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+
+namespace Solti.Utils.AppHost
+{
+    /// <summary>
+    /// Defines an abstract service registry.
+    /// </summary>
+    public interface IModuleRegistry
+    {
+        /// <summary>
+        /// Registers a module.
+        /// </summary>
+        public void Register<TInterface, TImplementation>() where TInterface : class where TImplementation : TInterface;
+    }
+}
