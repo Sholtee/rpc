feaedd67899646d8e5940cf06ce31bb8d4c03534
Dénes Solti <sodnaatx@gmail.com>
2020-07-01 06:38:01 +0200

    removed IRequestProcessor

--- SRC/AppHost/Private/RequestContext.cs
+++ SRC/AppHost/Private/RequestContext.cs
@@ -4,25 +4,50 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Solti.Utils.AppHost
 {
-    internal class RequestContext: IRequestContext
+    using Properties;
+
+    internal class RequestContext : IRequestContext
     {
-        public RequestContext(string? sessionId, string module, string method, string args) 
+        private RequestContext(string? sessionId, string module, string method, string args)
         {
             SessionId = sessionId;
-            Module    = module  ?? throw new ArgumentNullException(nameof(module));
-            Method    = method  ?? throw new ArgumentNullException(nameof(method));
-            Args      = args    ?? throw new ArgumentNullException(nameof(args));  
+            Module = module;
+            Method = method;
+            Args = args;
+        }
+
+        public static async Task<RequestContext> Create(HttpListenerRequest request) 
+        {
+            NameValueCollection queryString = request.QueryString;
+
+            return new RequestContext
+            (
+                queryString.Get("sessionid"),
+                queryString.Get("module") ?? throw new InvalidOperationException(Resources.NO_MODULE),
+                queryString.Get("method") ?? throw new InvalidOperationException(Resources.NO_METHOD),
+                await ReadBody()
+            );
+
+            async Task<string> ReadBody()
+            {
+                using var reader = new StreamReader(request.InputStream, request.ContentEncoding);
+                return await reader.ReadToEndAsync();
+            }
         }
 
         public string? SessionId { get; }
 
         public string Module { get; }
 
-        public string Method { get;  }
-        
+        public string Method { get; }
+
         public string Args { get; }
     }
 }
