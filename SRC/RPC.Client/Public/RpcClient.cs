5680f573ac410977b7d3af6b3a4ef9cb4d2eb990
Dénes Solti <sodnaatx@gmail.com>
2020-07-06 20:31:02 +0200

    RPC client initials III

--- SRC/RPC.Client/Public/RpcClient.cs
+++ SRC/RPC.Client/Public/RpcClient.cs
@@ -20,26 +20,78 @@ using Microsoft.AspNetCore.WebUtilities;
 namespace Solti.Utils.Rpc
 {   
     using Primitives;
+    using Properties;
     using Proxy;
     using Proxy.Generators;
     
     /// <summary>
     /// Contains the logic related to RPC service invocation
     /// </summary>
-    public class RpcClient<TInterface> : InterfaceInterceptor<TInterface> where TInterface : class
+    public class RpcClient<TInterface> where TInterface: class
     {
+        #region Private
         static RpcClient() 
         {
-            string cacheDir = Path.Combine(Path.GetTempPath(), ".rpc", typeof(RpcClient<TInterface>).Assembly.GetName().Version.ToString());
+            string cacheDir = Path.Combine(Path.GetTempPath(), $".rpcclient", typeof(RpcClient<TInterface>).Assembly.GetName().Version.ToString());
             Directory.CreateDirectory(cacheDir);
 
-            ProxyGenerator<TInterface, RpcClient<TInterface>>.CacheDirectory = cacheDir;
+            ProxyGenerator<TInterface, MethodCallForwarder>.CacheDirectory = cacheDir;
         }
 
-        private static string GetMemberId(MemberInfo member) => member.GetCustomAttribute<AliasAttribute>(inherit: false)?.Name ?? member.Name;
-
-        private async Task<object?> InvokeService(MethodInfo method, object[] args)
+        [SuppressMessage("Performance", "CA1812:Avoid uninstantiated internal classes")]
+        internal class MethodCallForwarder : InterfaceInterceptor<TInterface>
         {
+            public RpcClient<TInterface> Owner { get; }
+
+            public MethodCallForwarder(RpcClient<TInterface> owner) : base(null) => Owner = owner;
+
+            public override object? Invoke(MethodInfo method, object[] args, MemberInfo extra) => Owner.InvokeServicy(method, args);
+        }
+
+        [SuppressMessage("Reliability", "CA2008:Do not create tasks without passing a TaskScheduler")]
+        private static Task<T> Cast<T>(Task<object?> task) => task.ContinueWith(t => Task.FromResult((T) t.Result!)).Unwrap();
+
+        private static Task Cast(Task<object?> task, Type returnType) => (Task) Cache
+            .GetOrAdd(returnType, () =>
+            {
+                MethodInfo cast = ((MethodCallExpression)((Expression<Action>)(() => Cast<object>(null!))).Body)
+                    .Method
+                    .GetGenericMethodDefinition();
+
+                return cast.MakeGenericMethod(returnType).ToStaticDelegate();
+            })
+            .Invoke(new object[] { task });
+
+        private TInterface CreateProxy() => (TInterface) ProxyGenerator<TInterface, MethodCallForwarder>
+            .GeneratedType
+            .GetConstructor(new Type[] { typeof(RpcClient<TInterface>) })
+            .ToStaticDelegate()
+            .Invoke(new object[] { this });
+        #endregion
+
+        #region Protected
+        /// <summary>
+        /// Gets the ID of a member.
+        /// </summary>
+        protected virtual string GetMemberId(MemberInfo member)
+        {
+            if (member == null)
+                throw new ArgumentNullException(nameof(member));
+
+            return member.GetCustomAttribute<AliasAttribute>(inherit: false)?.Name ?? member.Name;
+        }
+
+        /// <summary>
+        /// Does the actual remote module invocation.
+        /// </summary>
+        protected virtual async Task<object?> InvokeServiceAsync(MethodInfo method, object[] args)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+
+            if (args == null)
+                throw new ArgumentNullException(nameof(args));
+
             using var data = new StringContent(JsonSerializer.Serialize(args), Encoding.UTF8, "application/json");
 
             using var client = new HttpClient();
@@ -61,51 +113,15 @@ namespace Solti.Utils.Rpc
             return result.Result;
         }
 
-        [SuppressMessage("Reliability", "CA2008:Do not create tasks without passing a TaskScheduler")]
-        private static Task<T> Cast<T>(Task<object?> task) => task.ContinueWith(t => Task.FromResult((T) t.Result!)).Unwrap();
-
-        private static Task Cast(Task<object?> task, Type returnType) => (Task) Cache
-            .GetOrAdd(returnType, () =>
-            {
-                MethodInfo cast = ((MethodCallExpression) ((Expression<Action>) (() => Cast<object>(null!))).Body)
-                    .Method
-                    .GetGenericMethodDefinition();
-
-                return cast.MakeGenericMethod(returnType).ToStaticDelegate();
-            })
-            .Invoke(new object[] { task } );
-
-        private static void ProcessRemoteError(ExceptionInfo exception) 
-        {
-            Type exceptionType = Type.GetType(exception.TypeName, throwOnError: false);
-
-            Exception? instance = null;
-
-            if (exceptionType != null)
-            {
-                Func<object?[], object>? ctor = exceptionType.GetConstructor(new[] { typeof(string) })?.ToStaticDelegate();
-
-                //
-                // Ha van string parameteru konstruktora akkor azt hasznaljuk
-                //
-
-                if (ctor != null) instance = (Exception) ctor.Invoke(new object?[] { exception.Message });
-            }
-
-            if (instance == null) instance = new RpcException(exception.Message);
-
-            throw instance;
-        }
-
         /// <summary>
-        /// See <see cref="InterfaceInterceptor{TInterface}.Invoke(MethodInfo, object[], MemberInfo)"/>
+        /// Does the actual remote module invocation.
         /// </summary>
-        public override object? Invoke(MethodInfo method, object[] args, MemberInfo extra)
+        protected virtual object? InvokeServicy(MethodInfo method, object[] args)
         {
             if (method == null)
                 throw new ArgumentNullException(nameof(method));
 
-            Task<object?> getResult = InvokeService(method, args);
+            Task<object?> getResult = InvokeServiceAsync(method, args);
 
             if (!typeof(Task).IsAssignableFrom(method.ReturnType))
                 //
@@ -115,7 +131,7 @@ namespace Solti.Utils.Rpc
 
                 return getResult.GetAwaiter().GetResult();
 
-            else 
+            else
             {
                 if (method.ReturnType == typeof(Task)) return getResult;
 
@@ -124,29 +140,53 @@ namespace Solti.Utils.Rpc
         }
 
         /// <summary>
+        /// Processes the <see cref="ExceptionInfo"/> returned by the remote host.
+        /// </summary>
+        protected virtual void ProcessRemoteError(ExceptionInfo exception) 
+        {
+            if (exception == null)
+                throw new ArgumentNullException(nameof(exception));
+
+            Func<object?[], object>? getRemoteException = Type
+                .GetType(exception.TypeName, throwOnError: false)
+                ?.GetConstructor(new[] { typeof(string) })
+                ?.ToStaticDelegate();
+
+            //
+            // Ha nem sikerult a tavoli kivetelt betolteni akkor sima Exception-t dobunk
+            //
+
+            Exception instance = (Exception?) getRemoteException?.Invoke(new object?[] { exception.Message }) ?? new Exception(exception.Message);
+
+            throw new RpcException(Resources.RPC_FAILED, instance);
+        }
+        #endregion
+
+        #region Public
+        /// <summary>
         /// The session ID related to this instance.
         /// </summary>
         public string? SessionId { get; }
 
         /// <summary>
-        /// The address of the remote host (e.g.: "www.mysite:1986/api").
+        /// The address of the remote host (e.g.: "www.mysite.com:1986/api").
         /// </summary>
         public string Host { get; }
-
-        internal RpcClient(string host, string? sessionId): base(null)
-        {
-            Host = host ?? throw new ArgumentNullException(nameof(host));
-            SessionId = sessionId;
-        }
 
         /// <summary>
         /// Creates a new <see cref="RpcClient{TInterface}"/> instance.
         /// </summary>
-        [SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "<Pending>")]
-        public static TInterface Create(string host, string? sessionId) => (TInterface) ProxyGenerator<TInterface, RpcClient<TInterface>>
-            .GeneratedType
-            .GetConstructor(new Type[] { typeof(string), typeof(string) })
-            .ToStaticDelegate()
-            .Invoke(new object?[] { host, sessionId });
+        public RpcClient(string host, string? sessionId)
+        {
+            Host = host ?? throw new ArgumentNullException(nameof(host));
+            SessionId = sessionId;
+            Proxy = CreateProxy();
+        }
+
+        /// <summary>
+        /// The generated proxy instance related to this client.
+        /// </summary>
+        public TInterface Proxy { get; }
+        #endregion
     }
 }
