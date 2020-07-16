b1d742faab2935684841763071a8615be8adf0f2
Dénes Solti <sodnaatx@gmail.com>
2020-07-16 16:18:00 +0200

    add RPC.Server.Sample project

---
+++ SRC/RPC.Server.Sample/Calculator.cs
@@ -0,0 +1,18 @@
+﻿/********************************************************************************
+* Calculator.cs                                                                 *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Solti.Utils.Rpc.Server.Sample
+{
+    public class Calculator : ICalculator
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public int Add(int a, int b) => a + b;
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public Task<int> AddAsync(int a, int b) => Task.FromResult(a + b);
+    }
+}
---
+++ SRC/RPC.Server.Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿/********************************************************************************
+* Program.cs                                                                    *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+namespace Solti.Utils.Rpc.Server.Sample
+{
+    class Program
+    {
+        static void Main()
+        {
+            using AppHost appHost = new AppHost();
+            appHost.Prepare();
+            appHost.Runner.Start();
+        }
+    }
+}
