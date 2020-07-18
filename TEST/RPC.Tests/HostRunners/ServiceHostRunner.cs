1287572a94120f2ad44defae8ca9037e7166990a
Dénes Solti <sodnaatx@gmail.com>
2020-07-18 16:24:06 +0200

    add a ServiceHostRunner test

---
+++ TEST/RPC.Tests/HostRunners/ServiceHostRunner.cs
@@ -0,0 +1,55 @@
+﻿/********************************************************************************
+* ServiceHostRunner.cs                                                          *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System.Diagnostics;
+using System.IO;
+
+using NUnit.Framework;
+
+namespace Solti.Utils.Rpc.Hosting.Tests
+{
+    using Server.Sample;
+
+    [TestFixture]
+    public class ServiceHostRunnerTests
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            InvokeExe(Path.ChangeExtension(typeof(ICalculator).Assembly.Location, "exe"), "-install");
+            InvokeExe("sc", "start Calculator");
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+            InvokeExe("sc", "stop Calculator");
+            InvokeExe(Path.ChangeExtension(typeof(ICalculator).Assembly.Location, "exe"), "-uninstall");
+        }
+
+        [Test]
+        public void InstalledService_ShouldRun() 
+        {
+            using var client = new RpcClient<ICalculator>("http://127.0.0.1:1986/api/");
+
+            Assert.That(client.Proxy.Add(1, 1), Is.EqualTo(2));
+        }
+
+        private static void InvokeExe(string exe, string args)
+        {
+            var psi = new ProcessStartInfo(exe)
+            {
+                Verb = "runas",
+                Arguments = args,
+                UseShellExecute = true
+            };
+
+            Process proc = Process.Start(psi);
+            proc.WaitForExit();
+
+            Assert.That(proc.ExitCode, Is.EqualTo(0));
+        }
+    }
+}
