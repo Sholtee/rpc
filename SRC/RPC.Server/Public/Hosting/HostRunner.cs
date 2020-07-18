7458b2f9c34b48492f9692197df7301352f237e2
Dénes Solti <sodnaatx@gmail.com>
2020-07-18 15:38:21 +0200

    HostRunner.static_ctor(): make runner registrations platform dependent

--- SRC/RPC.Server/Public/Hosting/HostRunner.cs
+++ SRC/RPC.Server/Public/Hosting/HostRunner.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Solti.Utils.Rpc.Hosting
 {
@@ -82,8 +83,12 @@ namespace Solti.Utils.Rpc.Hosting
         {
             Extend<DefaultHostRunner>();
             Extend<ConsoleHostRunner>();
-            Extend<InstallHostRunner_WinNT>();
-            Extend<ServiceHostRunner_WinNT>();
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Extend<InstallHostRunner_WinNT>();
+                Extend<ServiceHostRunner_WinNT>();
+            }
         }
 
         /// <summary>
