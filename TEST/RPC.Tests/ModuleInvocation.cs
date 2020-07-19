9649ed50571e9da7b0a77192094e001cae3fa12b
Dénes Solti <sodnaatx@gmail.com>
2020-07-19 17:53:18 +0200

    [tests] ModuleInvocation.cs: using cleanup

--- TEST/RPC.Tests/ModuleInvocation.cs
+++ TEST/RPC.Tests/ModuleInvocation.cs
@@ -4,7 +4,6 @@
 * Author: Denes Solti                                                           *
 ********************************************************************************/
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
