bcc521ce7a98fc00572a16318451656c24c35967
Dénes Solti <sodnaatx@gmail.com>
2021-02-28 17:05:53 +0100

    IParameterValidator.cs: using cleanup

--- SRC/RPC.Interfaces/Interfaces/IParameterValidator.cs
+++ SRC/RPC.Interfaces/Interfaces/IParameterValidator.cs
@@ -3,7 +3,6 @@
 *                                                                               *
 * Author: Denes Solti                                                           *
 ********************************************************************************/
-using System;
 using System.Reflection;
 
 namespace Solti.Utils.Rpc.Interfaces
