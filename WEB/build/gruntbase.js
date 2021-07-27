9d5ec6d7ade8a90c9873f79dfe29cbc47f35218e
Dénes Solti <sodnaatx@gmail.com>
2021-07-27 06:27:46 +0200

    try to merge coverage reports I

--- WEB/build/gruntbase.js
+++ WEB/build/gruntbase.js
@@ -172,7 +172,8 @@ module.exports = ({task, registerTask, initConfig, file, template, option}, dir)
             coveralls: {
                 COVERALLS_SERVICE_NAME: 'appveyor',
                 COVERALLS_GIT_BRANCH: () => process.env.APPVEYOR_REPO_BRANCH,
-                COVERALLS_SERVICE_JOB_ID: () => process.env.APPVEYOR_JOB_ID
+                COVERALLS_SERVICE_JOB_ID: () => process.env.APPVEYOR_JOB_ID,
+                COVERALLS_PARALLEL: true
             }
         },
         coveralls: {
