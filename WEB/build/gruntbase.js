a2e2ac556c28e247a09257fab0fc4cf738c46a5f
Dénes Solti <sodnaatx@gmail.com>
2021-08-12 06:30:11 +0200

    add some debug codes (for build scripts)

--- WEB/build/gruntbase.js
+++ WEB/build/gruntbase.js
@@ -172,7 +172,8 @@ module.exports = ({task, registerTask, initConfig, file, template, option}, dir)
             coveralls: {
                 COVERALLS_SERVICE_NAME: 'appveyor',
                 COVERALLS_GIT_BRANCH: () => process.env.APPVEYOR_REPO_BRANCH,
-                COVERALLS_SERVICE_JOB_ID: () => process.env.APPVEYOR_JOB_ID
+                COVERALLS_SERVICE_JOB_ID: () => process.env.APPVEYOR_JOB_ID,
+                NODE_COVERALLS_DEBUG: () => process.end.DEBUG_CI ? 1 : 0
             }
         },
         coveralls: {
