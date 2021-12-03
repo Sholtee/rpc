acdd12524cd44dd0375ca873cce855ed6fefa2b0
Dénes Solti <sodnaatx@gmail.com>
2021-12-03 17:01:35 +0100

    AsyncExtensions.cs: improved performance

--- SRC/RPC.Server/Private/Extensions/AsyncExtensions.cs
+++ SRC/RPC.Server/Private/Extensions/AsyncExtensions.cs
@@ -5,6 +5,7 @@
 ********************************************************************************/
 using System;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Solti.Utils.Rpc.Internals
@@ -13,6 +14,16 @@ namespace Solti.Utils.Rpc.Internals
 
     internal static class AsyncExtensions
     {
+        private static readonly MethodInfo FGenericJoin = ((Func<Func<Task>, Func<Task>, Task<object>>) Join<object>)
+            .Method
+            .GetGenericMethodDefinition();
+
+        private static async Task<T> Join<T>(Func<Task> decorator, Func<Task> original)
+        {
+            await decorator();
+            return await (Task<T>) original();
+        }
+
         //
         // Ha kulonbozo visszateresu Task-okat akarunk osszefuzni akkor a TaskExtensions.Unwrap()
         // metodus nem jatszik
@@ -33,20 +44,11 @@ namespace Solti.Utils.Rpc.Internals
 
             if (typeof(Task).IsAssignableFrom(returnType)) // Task<>
             {
-                Func<Task<object>> invokeAsync = InvokeAsync<object>;
-
-                return (Task) invokeAsync
-                    .Method
-                    .GetGenericMethodDefinition()
+                Func<object?[], object> join = Cache.GetOrAdd(returnType, () => FGenericJoin
                     .MakeGenericMethod(returnType.GetGenericArguments().Single())
-                    .ToInstanceDelegate()
-                    .Invoke(invokeAsync.Target, Array.Empty<object?>());
+                    .ToStaticDelegate());
 
-                async Task<T> InvokeAsync<T>()
-                {
-                    await decorator();
-                    return await (Task<T>) original();
-                }
+                return (Task) join(new object[] { decorator, original });
             }
 
             throw new NotSupportedException();
