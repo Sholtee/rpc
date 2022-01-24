13a203a1367a78dc8d7e60b0a11aa49ddd01607a
Dénes Solti <sodnaatx@gmail.com>
2022-01-24 07:31:51 +0100

    move ModuleInvocationExtensions to a separate file

---
+++ SRC/RPC.Server/Private/Extensions/ModuleInvocationExtensions.cs
@@ -0,0 +1,24 @@
+﻿/********************************************************************************
+* ModuleInvocation.cs                                                           *
+*                                                                               *
+* Author: Denes Solti                                                           *
+********************************************************************************/
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Solti.Utils.Rpc.Internals
+{
+    /// <summary>
+    /// Defines some extensions to the <see cref="ModuleInvocation"/> delegate.
+    /// </summary>
+    public static class ModuleInvocationExtensions // "delegate"-bol nem szarmazhatunk ezert ez a megoldas
+    {
+        internal static IDictionary<ModuleInvocation, IReadOnlyList<Type>> RelatedModules { get; } = new ConcurrentDictionary<ModuleInvocation, IReadOnlyList<Type>>();
+
+        /// <summary>
+        /// Gets the registered modules related to this <see cref="ModuleInvocation"/> instance.
+        /// </summary>
+        public static IReadOnlyList<Type> GetRelatedModules(this ModuleInvocation src) => RelatedModules[src ?? throw new ArgumentNullException(nameof(src))];
+    }
+}
--- SRC/RPC.Server/Private/ModuleInvocation.cs
+++ SRC/RPC.Server/Private/ModuleInvocation.cs
@@ -29,19 +29,6 @@ namespace Solti.Utils.Rpc.Internals
     public delegate Task<object?> ModuleInvocation(IInjector injector, IRpcRequestContext context, JsonSerializerOptions serializerOptions);
 
     /// <summary>
-    /// Defines some extensions to the <see cref="ModuleInvocation"/> delegate.
-    /// </summary>
-    public static class ModuleInvocationExtensions // "delegate"-bol nem szarmazhatunk ezert ez a megoldas
-    {
-        internal static IDictionary<ModuleInvocation, IReadOnlyList<Type>> RelatedModules { get; } = new ConcurrentDictionary<ModuleInvocation, IReadOnlyList<Type>>();
-
-        /// <summary>
-        /// Gets the registered modules related to this <see cref="ModuleInvocation"/> instance.
-        /// </summary>
-        public static IReadOnlyList<Type> GetRelatedModules(this ModuleInvocation src) => RelatedModules[src ?? throw new ArgumentNullException(nameof(src))];
-    }
-
-    /// <summary>
     /// Builds <see cref="ModuleInvocation"/> instances.
     /// </summary>
     public class ModuleInvocationBuilder: IBuilder<ModuleInvocation>
