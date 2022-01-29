c54cb94c11b4f486306b2fb6b6e3a89a119f21fa
Dénes Solti <sodnaatx@gmail.com>
2022-01-29 13:32:27 +0100

    ValidationException.cs fix [TargetName is not serialized]

--- SRC/RPC.Interfaces/Exceptions/ValidationException.cs
+++ SRC/RPC.Interfaces/Exceptions/ValidationException.cs
@@ -12,10 +12,14 @@ namespace Solti.Utils.Rpc.Interfaces
     /// </summary>
     public class ValidationException : Exception
     {
+        //
+        // Kivetelek nem kerulnek egy az egyben szerializalasra, ezert Data-ba mentunk (az biztosan eljut a kliensig).
+        //
+
         /// <summary>
         /// The name of parameter or property on which the validation failed.
         /// </summary>
-        public string? TargetName { get; set; }
+        public string? TargetName { get => (string?) Data[nameof(TargetName)]; set => Data[nameof(TargetName)] = value; }
 
         /// <summary>
         /// Creates a new <see cref="ValidationException"/> instance.
